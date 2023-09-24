9ace98249d1f7e04e1dea870b98ccba2d77eeb9b
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2023-09-24 20:12:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from "./components/Header/Header"
+import Body from "./components/Body/Body"
+import "./App.css"
+const Apps = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  )
+}
+
+export default Apps
