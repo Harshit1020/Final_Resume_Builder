80d612e274d1bc360eafe47ac29fd4395317cc02
Harshit_Dongre <95414266+Harshit1020@users.noreply.github.com>
2023-09-26 12:29:04 +0530

    About add

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,24 @@
 import React from 'react'
+import css from "./About.module.css"
+import { Link } from 'react-router-dom'
 
 const About = () => {
   return (
-    <div className='container mt-5'>
-    <br /><br />
-    About Page</div>
+    <div className={css.abc}>
+      <div className={css.txt}><h1>About us</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero animi consectetur quo a ipsum magnam excepturi accusantium? Blanditiis provident possimus eos vero modi fugit et sit laboriosam. Quisquam, qui iure.
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic necessitatibus tempore dignissimos, magnam praesentium obcaecati perspiciatis, ipsa molestiae distinctio iure unde veritatis! A nemo aliquid, perspiciatis quam facere magnam vero repellat dolore molestias asperiores magni autem nam distinctio qui fuga?
+        </p>
+      </div>
+    <div className={css.icon}>
+
+      <Link>Facebook</Link>
+      <Link>Instagram</Link>
+      <Link>Twitter</Link>
+      <Link>Linkedln</Link>
+
+    </div>
+    </div>
   )
 }
 export default About
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -33,7 +33,6 @@ const Resume = forwardRef((props, ref) => {
   const getFormattedDate = (value) => {
     if (!value) return "";
     const date = new Date(value);
-
     return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
   };
 
